6f25717ab6aabf8d2b922f00bd4775f334e4b999
moonpoison <moonpoison@naver.com>
2024-01-27 23:27:12 +0900

    Update [Array / String] - String Compression

--- moonpoison/array_string/443.cpp
+++ moonpoison/array_string/443.cpp
@@ -1,36 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    vector<char> chars;
-    int c;
-    cin>>c;
-    for(int i=0;i<c;++i)
+class Solution {
+public:
+    int compress(vector<char>& chars) 
     {
-        char h;
-        cin>>h;
-        chars.push_back(h);
-    }
-    sort(chars.begin(), chars.end());
-    vector<char> ans;
-    int a=1;
-    char b=chars[0];
-    for(int i=1;i<chars.size();++i)
-    {
-        if(b==chars[i]) a++;
-        else
+        vector<char> ans;
+        int a=1;
+        char b=chars[0];
+        for(int i=1;i<chars.size();++i)
         {
-            ans.push_back(b);
-            ans.push_back(a+48);
-            a=1;
-            b=chars[i];
+            if(b==chars[i]) a++;
+            else
+            {
+                ans.push_back(b);
+                if(a!=1)
+                {
+                    for(const char c : to_string(a))  
+                        ans.push_back(c);
+                } 
+                a=1;
+                b=chars[i];
+            }
         }
+        ans.push_back(b);
+        if(a!=1)
+        {
+            for(const char c : to_string(a))  
+                ans.push_back(c);
+        } 
+        chars.swap(ans);
+        return chars.size();
     }
-    ans.push_back(b);
-    ans.push_back(a+48);
-    chars.swap(ans);
-    for(auto &i:chars)
-    {
-        cout<<i;
-    }
-}
+};
