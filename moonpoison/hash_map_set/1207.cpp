5df9ad75c763c4501f5011b9d847fb3e366b4b11
moonpoison <moonpoison@naver.com>
2024-02-13 09:45:56 +0900

    [Hash Map / Set] - Unique Number of Occurrences

---
+++ moonpoison/hash_map_set/1207.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    map<int, int> m;
+    int a[1001]={0};
+    for(int i=0;i<arr.size();++i)
+        m[arr[i]]++;
+    for(auto i=m.begin();i!=m.end();++i)
+    {
+        a[i->second]++;
+        if(a[i->second]>1) return false;
+    }
+    return true;
+}
