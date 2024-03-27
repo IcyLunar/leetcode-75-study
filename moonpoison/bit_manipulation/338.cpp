9c8bfa140c1aa85ed2dd2df738d8a8a63f3ab968
moonpoison <moonpoison@naver.com>
2024-03-27 15:40:15 +0900

    [Bit Manipulation] - Counting Bits

---
+++ moonpoison/bit_manipulation/338.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> countBits(int n) 
+    {
+        vector<int> ans(n+1);
+        ans[0]=0;
+        for(int i=1;i<=n;++i)
+            ans[i]=ans[i/2]+i%2;
+        return ans;
+    }
+};
